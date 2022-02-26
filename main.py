185c3f87ce183f0a8a8f3c5c9e4cd2e6435c696d
M-Salti <mohamed.al.salti.96@gmail.com>
2022-02-26 13:56:58 +0200

    Make the solver generic and add 'yellow' info

--- main.py
+++ main.py
@@ -1,30 +1,29 @@
 import re
 from collections import Counter
-from functools import lru_cache
 from pathlib import Path
 
-from pipe import dedup, select, where
+from pipe import where
+from pprint import pprint
 
 
-@lru_cache
-def get_all_words():
-    return list(
-        Path("/usr/share/dict/words").open().read().splitlines()
-        | select(lambda x: x.lower())
-        | dedup
-        | where(lambda x: re.match("[a-z]{5}$", x))
-    )
+def read_words(file: str) -> list[str]:
+    return Path(file).open(encoding="utf8").read().splitlines()
 
 
-def make_suggestions(cur_state: str, rejected_chars: str, contains: str):
+def make_suggestions(possible_words: list[str], cur_state: str, grey: str, yellow: str):
     unknown_positions = [i for i in range(5) if cur_state[i] == "."]
     unknown_chars = lambda x: set(x[i] for i in unknown_positions)
 
+    grey_set = set(grey)
+    yellow_set = set(ch for ch in yellow if ch != ".")
+    # yellow_pos = set(enumerate(yellow))
+
     words = list(
-        get_all_words()
+        possible_words
         | where(lambda x: re.match(cur_state, x))
-        | where(lambda x: all(r not in unknown_chars(x) for r in rejected_chars))
-        | where(lambda x: all(c in unknown_chars(x) for c in contains))
+        | where(lambda x: unknown_chars(x) & grey_set == set())
+        | where(lambda x: unknown_chars(x) & yellow_set == yellow_set)
+        | where(lambda x: set(enumerate(x)) & set(enumerate(yellow)) == set())
     )
 
     counters = [Counter(w[i] for w in words) for i in unknown_positions]
@@ -39,7 +38,9 @@ def make_suggestions(cur_state: str, rejected_chars: str, contains: str):
     return suggestions
 
 
-cur_state = "....."
-rejected = ""
-contains = ""
-print(make_suggestions(cur_state, rejected, contains)[0])
+if __name__ == "__main__":
+    cur_state = "....."
+    grey = ""
+    yellow = "....."
+    # print top 10 suggesttions
+    pprint(make_suggestions(read_words("./data/wordle.main.txt"), cur_state, grey, yellow)[:10])
